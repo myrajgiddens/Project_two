96606ce89391c98193ea5ebe18cb66c362965eb9
Myra Giddens <myrajgiddens@aol.com>
2020-05-04 12:25:35 -0400

    trying to get giphy results updated

--- scripts.js
+++ scripts.js
@@ -1,49 +1,53 @@
 //   ansxed5p3AGVsc5TPLocIqLK9vNnfTFd
-function displayResults (results) {
+function displayResults(results) {
 
     let myHtml = "<div class='row'>";
-    for(res of results){
-      // console.log(res);
-      myHtml = myHtml + `
+    for (res of results) {
+        // console.log(res);
+        myHtml = myHtml + `
       <div class='col-sm-4'>
         <img src=${res.images.original.url} />
       </div>`
     }
-  
+
     myHtml = myHtml + "</div>"
-  
+
     console.log(myHtml);
     $('#results').html(myHtml);
-  
-  }
-  
-  function fetchDataFromGiphy (searchTerm) {
+
+}
+
+function fetchDataFromGiphy(searchTerm) {
     let url = `https://api.giphy.com/v1/gifs/search?api_key=ansxed5p3AGVsc5TPLocIqLK9vNnfTFd&q=${searchTerm}`
     console.log(url);
-  
+
     fetch(url)
-    .then(function(response) {
-      return response.json();
-    }).then(function(result){
-      // console.log(result.data);
-      displayResults(result.data)
-    })
-  
-  }
-  
-  function loadApp() {
+        .then(function (response) {
+            return response.json();
+        }).then(function (result) {
+            // console.log(result.data);
+            displayResults(result.data)
+        })
+
+}
+
+
+
+function loadApp() {
+    $('#button').on('click', function () {
+        console.log('Get me results!');
+        fetchTheResults('#searchbar');
+    });
     console.log('Page is loaded!');
     console.log('Jquery is accessible', $);
-  
+
     // get data from the form that the user user types in
-  
-    let searchTerm = 'pixar';
-    fetchDataFromGiphy(searchTerm);
-  
-  
-  }
-  
-  
-  
-  
-  $(loadApp);
+
+    let searchTerm = 'disney';
+    fetchDataFromGiphy(#search)
+}
+
+
+
+
+$(loadApp);
