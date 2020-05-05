15ee2eeb132ee0cb49f97fd7bf65e243bdf4eedf
Myra Giddens <myrajgiddens@aol.com>
2020-05-05 13:17:44 -0400

    Search now fetching results/form is working

--- scripts.js
+++ scripts.js
@@ -3,56 +3,73 @@
 // const searchUrl='https://api.giphy.com/v1/gifs/search'
 
 
-// function displayResults(results) {
 
-//     let myHtml = "<div class='row'>";
-//     for (res of results) {
-//         // console.log(res);
-//         myHtml = myHtml + `
-//       <div class='col-sm-4'>
-//         <img src=${res.images.original.url} />
-//       </div>`
-//     }
+// function displayResults (results) {
 
-//     myHtml = myHtml + "</div>"
+//   let myHtml = "<div class='row'>";
+//   for(res of results){
+//     // console.log(res);
+//     myHtml = myHtml + `
+//     <div class='col-sm-4'>
+//       <img src=${res.images.original.url} />
+//     </div>`
+//   }
 
-//     console.log(myHtml);
-//     $('#results').html(myHtml);
+//   myHtml = myHtml + "</div>"
 
-// }
-
-// function fetchDataFromGiphy(searchTerm) {
-//     let url = `https://api.giphy.com/v1/gifs/search?api_key=4zv45wmD5vJvLn2UE3B4tNBY1ny9kZZV&q=$('#search')`
-//     console.log(url);
-
-    
-//     fetch(url)
-//         .then(function (response) {
-//             return response.json();
-//         }).then(function (result) {
-//             // console.log(result.data);
-//             displayResults(result.data)
-//         })
+//   console.log(myHtml);
+//   $('#results').html(myHtml);
 
 // }
 
-// document.getElementById("myForm").addEventListener("click", checkForm, false);
 
-// function loadApp() {
-//     $('#button').on('click', function () {
-//         console.log('Get me results!');
-//         fetchTheResults('#search');
-//         document.getElementById(elementId: 'search');
-//     });
-//     console.log('Page is loaded!');
-//     console.log('Jquery is accessible', $);
 
-//     let searchTerm = 'disney';
-//     fetchDataFromGiphy(searchTerm);
+// function fetchDataFromGiphy (searchTerm) {
+//   let url = `https://api.giphy.com/v1/gifs/search?api_key=4zv45wmD5vJvLn2UE3B4tNBY1ny9kZZV&q=${searchTerm}`;
+//   console.log(url);
+
+//   // $.ajax
+//   // $.getJSON
+
+//   fetch(url)
+//   .then(function(response) {
+//     return response.json();
+//   }).then(function(result){
+//     // console.log(result.data);
+//     displayResults(result.data)
+//   })
+
 // }
 
-// $(loadApp);
 
+// function bootApp() {
+//   console.log('Page is loaded!');
+//   console.log('Jquery is available', $);
+
+//   // get data from the form that the user user types in
+
+//   let searchTerm = 'disney';
+//   fetchDataFromGiphy(searchTerm)
+// }
+
+// function getSearchTerm() {
+//   console.log
+// }
+
+// // $('#myForm').on('submit'() {
+// //   event.preventDefault
+// //   let searchTerm=$('#searchTerm').val
+// //   console.log(searchTerm);
+// // })
+
+
+
+// $(bootApp)
+
+
+// //   ansxed5p3AGVsc5TPLocIqLK9vNnfTFd
+// const apiKey='ansxed5p3AGVsc5TPLocIqLK9vNnfTFd'
+// const searchUrl='https://api.giphy.com/v1/gifs/search'
 
 
 
@@ -74,8 +91,10 @@ function displayResults (results) {
 
 }
 
+
+
 function fetchDataFromGiphy (searchTerm) {
-  let url = `https://api.giphy.com/v1/gifs/search?api_key=4zv45wmD5vJvLn2UE3B4tNBY1ny9kZZV&q=${searchTerm}`
+  let url = `https://api.giphy.com/v1/gifs/search?api_key=4zv45wmD5vJvLn2UE3B4tNBY1ny9kZZV&q=${searchTerm}`;
   console.log(url);
 
   // $.ajax
@@ -93,18 +112,27 @@ function fetchDataFromGiphy (searchTerm) {
 
 
 function bootApp() {
-  console.log('Page is loaded up!');
+  console.log('Page is loaded!');
   console.log('Jquery is available', $);
 
   // get data from the form that the user user types in
+  $('#myForm').on('submit', (event) => {
+    event.preventDefault();
+    let searchTerm=$('#searchTerm').val();
+    console.log(searchTerm);
+    fetchDataFromGiphy(searchTerm);
+  });
 
-  let searchTerm = 'disney';
-  fetchDataFromGiphy(searchTerm)
+  // let searchTerm = 'disney';
+  
+}
 
-
+function getSearchTerm() {
+  console.log
 }
 
 
 
 
-$(bootApp);
+
+$(bootApp)
